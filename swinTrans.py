7c56b4a990d6c0d7f07fdb61010c8caea5f42623
khiemict0902 <khiem0902@gmail.com>
2024-09-30 21:53:56 +0700

    update swinTrans.py

--- swinTrans.py
+++ swinTrans.py
@@ -1,21 +1,21 @@
 import torch
 import torchvision.models as models
 import torch.nn as nn
+from torchvision import datasets, models, transforms
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
 
-# Check if GPU is available
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-model = models.swin_t(pretrained=True)
+model = models.swin_t()
+print(model)
 
-# Modify the classifier head to match the number of classes in your dataset
+
 num_classes = 10
 model.head = nn.Linear(model.head.in_features, num_classes)
 
 # Move the model to the GPU if available
 model = model.to(device)
-import torchvision.transforms as transforms
-from torchvision import datasets
-from torch.utils.data import DataLoader
 
 transform = transforms.Compose([
     transforms.Resize((224, 224)),  
@@ -25,19 +25,24 @@ transform = transforms.Compose([
 
 
 train_dataset = datasets.ImageFolder(root='./2750', transform=transform)
-test_dataset = datasets.ImageFolder(root='./2750', transform=transform)
+# test_dataset = datasets.ImageFolder(root='./2750', transform=transform)
 
+print(train_dataset)
 
 train_loader = DataLoader(dataset=train_dataset, batch_size=32, shuffle=True, num_workers=2)
-test_loader = DataLoader(dataset=test_dataset, batch_size=32, shuffle=False, num_workers=2)
+# test_loader = DataLoader(dataset=test_dataset, batch_size=32, shuffle=False, num_workers=2)
 
 criterion = nn.CrossEntropyLoss()
 
 
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-4, weight_decay=1e-4)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.0001, weight_decay=0.0001)
+
+lossNums = []
+acc = []
 
 def train_model(model, train_loader, criterion, optimizer, device, num_epochs=10):
-    model.train()  # Set the model to training mode
+    model.train()  
+    
     for epoch in range(num_epochs):
         running_loss = 0.0
         correct_predictions = 0
@@ -47,7 +52,6 @@ def train_model(model, train_loader, criterion, optimizer, device, num_epochs=10
             # Move images and labels to the device (GPU/CPU)
             images, labels = images.to(device), labels.to(device)
             
-            # Zero the parameter gradients
             optimizer.zero_grad()
             
             # Forward pass
@@ -60,6 +64,7 @@ def train_model(model, train_loader, criterion, optimizer, device, num_epochs=10
             
             # Track metrics
             running_loss += loss.item() * images.size(0)
+            print(running_loss)
             _, predicted = torch.max(outputs, 1)
             correct_predictions += (predicted == labels).sum().item()
             total_samples += labels.size(0)
@@ -67,9 +72,15 @@ def train_model(model, train_loader, criterion, optimizer, device, num_epochs=10
         # Print statistics for the epoch
         epoch_loss = running_loss / total_samples
         accuracy = correct_predictions / total_samples
+        lossNums.append(epoch_loss)
+        acc.append(accuracy)
         print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {epoch_loss:.4f}, Accuracy: {accuracy:.4f}')
 
-# Train the model for 10 epochs
 train_model(model, train_loader, criterion, optimizer, device, num_epochs=10)
 torch.save(model.state_dict(),'out.pth')
+
+plt.plot(10,lossNums)
+plt.show()
+
+
 
